02ef14bace38cfe2b0f374648b59b1fbec1651e1
Alejo Castaño <alejocas98@gmail.com>
2018-09-22 11:35:47 -0500

    Validation done

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		if(isset($_GET['nombre'])){
 			$nombre = $_GET['nombre'];
 		} else {
-			$nombre = "¿Quieres ingresar un nombre?";
+			$nombre = "¿Quieres ingresar un nombre?, prueba añadiendo a la URL este código: ?nombre=Elkin";
 		}
 		?>
 		<p>Hola mundo -> <?php echo $nombre ?> :D</p>
